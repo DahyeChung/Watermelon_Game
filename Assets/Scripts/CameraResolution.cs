7714b91868d826b366a5ae8b20f29c5a24088cd8
DahyeChung <lewachung@gmail.com>
2023-12-14 21:38:01 +0900

    System : camera resolution

--- Assets/Scripts/CameraResolution.cs
+++ Assets/Scripts/CameraResolution.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraResolution : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheight = ((float)(rect.width / rect.height)) / ((float)9 / 16);
+        float scalewidth = 1f / scaleheight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (scaleheight < 1)
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        camera.rect = rect;
     }
 }
