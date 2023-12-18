d35e010ec83aff88db47091d68fc1dda2a23a8ec
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-12-19 01:58:10 +0900

    UI: Redo
    panel pop up & restart button
    redo

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,7 +12,15 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     //public TextMeshProUGUI scoreTextGameOver;
     public UIanim UIanim;
 
+    //ccy추가
 
+    // 설정 패널 컨트롤러에 대한 참조
+    public SettingsPanelController settingsPanelController;
+
+    // 게임이 일시 정지 상태인지 추적
+    private bool isPaused = false;
+
+    //ccy
 
     public void AddScore(int score)
     {
@@ -40,4 +48,49 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         UIanim.GameOverUI();
         Debug.Log("Game Over");
     }
+
+    //ccy
+    // 일시 정지 및 재개를 위한 메소드
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            // 게임 재개
+            ResumeGame();
+        }
+        else
+        {
+            // 게임 일시 정지
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0; // 게임 시간을 정지
+
+        // 배경 음악 일시 정지
+        SoundManager.Instance.PauseBGM();
+    }
+
+    void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1; // 게임 시간을 정상으로 재개
+
+        // 배경 음악 재개
+        SoundManager.Instance.ResumeBGM();
+
+        // 설정 패널을 숨깁니다.
+        if (settingsPanelController != null)
+        {
+            settingsPanelController.settingsPanel.SetActive(false);
+        }
+
+        Debug.Log("turn off setting panel");
+
+    }
+
+    //ccy
 }
